a6f3f400899d3ab9b674f279a8c4f3878f1b214d
Devanshu Joshi <iamdevanshu007@gmail.com>
2025-07-12 11:06:28 +0530

    Add UserRepository interface for MongoDB operations

---
+++ ReWearBackend/src/main/java/org/rewear/DataRepository/UserRepository.java
@@ -0,0 +1,12 @@
+package org.rewear.DataRepository;
+
+import org.rewear.models.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByUsername(String username);
+    List<User> findByRole(String role);
+}
