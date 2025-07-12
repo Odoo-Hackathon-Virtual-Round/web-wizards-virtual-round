7175810ae24380cf54e3154c009cd4b0686eaa28
Devanshu Joshi <iamdevanshu007@gmail.com>
2025-07-12 12:50:53 +0530

    Added Working Functionality

--- ReWearBackend/src/main/java/org/rewear/services/UserServiceImpl.java
+++ ReWearBackend/src/main/java/org/rewear/services/UserServiceImpl.java
@@ -1,7 +1,10 @@
 package org.rewear.services;
 
 import org.rewear.DataRepository.UserRepository;
+import org.rewear.models.AdminUser;
+import org.rewear.models.RegularUser;
 import org.rewear.models.User;
+import org.rewear.models.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -28,10 +31,31 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public String setUser(User user) {
-        if(userRepository.findByUsername(user.getUsername()).isPresent())
+    public String setUser(UserDTO dto) {
+        if(userRepository.findByUsername(dto.getUsername()).isPresent())
             return "Username already exist";
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        User user;
+
+        if ("USER".equalsIgnoreCase(dto.getRole())) {
+            RegularUser ru = new RegularUser();
+            ru.setUsername(dto.getUsername());
+            ru.setEmail(dto.getEmail());
+            ru.setPassword(passwordEncoder.encode(dto.getPassword()));
+            ru.setRole("USER");
+            ru.setPoints(dto.getPoints() != null ? dto.getPoints() : 0);
+            user = ru;
+        } else if ("ADMIN".equalsIgnoreCase(dto.getRole())) {
+            AdminUser admin = new AdminUser();
+            admin.setUsername(dto.getUsername());
+            admin.setEmail(dto.getEmail());
+            admin.setPassword(passwordEncoder.encode(dto.getPassword()));
+            admin.setRole("ADMIN");
+            user = admin;
+        } else {
+            return "Invalid role";
+        }
+
         userRepository.save(user);
         return "User Created Successfully";
     }
