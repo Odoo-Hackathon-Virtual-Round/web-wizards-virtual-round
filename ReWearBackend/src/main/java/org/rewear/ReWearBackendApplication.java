63ca5807947575846cfdd4d58636f519925ecdb2
Hetvirani <hetvirani1305@gmail.com>
2025-07-12 10:02:15 +0530

    backend Project creatred

---
+++ ReWearBackend/src/main/java/org/rewear/ReWearBackendApplication.java
@@ -0,0 +1,13 @@
+package org.rewear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReWearBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReWearBackendApplication.class, args);
+    }
+
+}
